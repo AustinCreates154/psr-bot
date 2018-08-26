c8321b0982177cc2fb3153d6f5356faba20e5be9
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-26 12:33:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,24 @@
 const discord = require('discord.js');
-var client = new discord.Client();
-const config = require('./config.json');
-var prefix = ".",
+const client = new discord.Client();
+const config = require(`./config.json`)
+var prefix = "."
 
 
 // ===Loading commands===
 
-var client.commands = new discord.Collection()
+client.commands = new discord.Collection()
 
 require('fs').readdir("./commands/", (err, files) => {
   console.log("Loading commands...");
   if (err) return console.log(`Command loading failed!`);
   files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
-    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+    client.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
   });
 });
 
 // ===Done Loading commands===
 
-client.on('guildMemberAdd', (member) => require('./events/guildMemberAdd.js')(bot, member))
+client.on('guildMemberAdd', (member) => require('./events/guildMemberAdd.js')(client, member))
 
 client.on('ready', () => {
   var statuses = ["over Himiachi Base", "bot moosic", "bot gamez"]
@@ -46,7 +46,7 @@ client.on('ready', () => {
 client.on('message', message => {
   let mArray = message.content.split(" ")
   let args = mArray.slice(1)
-  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  let cmd = client.commands.get(mArray[0].slice(prefix.length))
   if (message.author.bot) return;
   if (message.channel.type == "dm") return;
   if (!message.content.startsWith(prefix)) return;
