414ece19c5957a7e6f2d84a85d622e7bb3fa9fe4
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-01 01:54:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,2 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+
+bot.on("guildMemberAdd" member => {
+  console.log(member.user.username + "Has joined PSR")
+})
+bot.on("ready", () => {
+  console.log('PSR bot is locked and loaded')
+})
+bot.login(process.evl.TOKEN)
